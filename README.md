459c60aaef1f7fa7b128a32f5ba406d6cb0441ca
Sven Hübner <sven.huebner.it@gmx.de>
2023-08-28 06:50:28 +0200

    feat: add key validation (mandatory and unique)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Summary
+
+Checks referential integrity of a JSON document according to an external specifications similar to XSD's keyref etc.
---
+++ src/validateRels.js
@@ -0,0 +1,95 @@
+const jp = require('jsonpath')
+
+function validateRels(objToValidate, relationsSpec) {
+  const { relations } = relationsSpec
+  const relationIds = Object.keys(relations)
+  const keyIds = relationIds.filter(id => relations[id].key)
+  const keyRefIds = relationIds.filter(id => relations[id].keyRef)
+
+  const keyDiagnostics = keyIds.flatMap(id => validateKey(id, objToValidate, relations))
+
+  return keyDiagnostics
+}
+
+function validateKey(id, objToValidate, relations) {
+  const keyDefinition = relations[id].key
+  const { selector, field } = keyDefinition
+  const selectedNodes = jp.nodes(objToValidate, selector);
+  const selectedFields = selectedNodes.map(node => {
+    return {
+      ...node,
+      fieldNodes: jp.nodes(node.value, field)
+    }
+  })
+
+  const fieldsWithoutKey = selectedFields.filter(f => f.fieldNodes.length == 0)
+  const fieldWithoutKeyDiagnostics = fieldsWithoutKey.map(node => {
+    const missingFieldName = formatField(field)
+
+    return {
+      instancePath: formatPathToNode(node),
+      keyword: "key",
+      message: `property '${missingFieldName}' is required`,
+      params: {
+        keyId: id,
+        missingProperty: missingFieldName
+      }
+    }
+  })
+  
+  const fieldsWithKey = selectedFields.filter(f => !fieldsWithoutKey.includes(f))
+  const groups = groupBy(fieldsWithKey, f => f.fieldNodes[0].value)
+
+  const duplicateGroups = Object.entries(groups).filter(([, value]) => value.length > 1)
+
+  const duplicateKeyDiagnostics = duplicateGroups.map(([key, node]) => {return {
+    instancePath: formatSelector(selector),
+    keyword: "key",
+    message: `property '${node[0].fieldNodes[0].path.slice(1)}' must be unique`,
+    params: {
+      keyId: id,
+      duplicates: node.map(formatPathToNode)
+    }
+  }})
+
+  return fieldWithoutKeyDiagnostics.concat(duplicateKeyDiagnostics)
+}
+
+function formatPathToNode(node) {
+  return '/' + node.path.slice(1).join('/')
+}
+
+function formatSelector(selector) {
+  const converted = selector.replace('$', '').replace('.', '/').replace(/\[\d*?:\d*?(:\d*?)?\]/, '/')
+  if (converted[converted.length - 1] === '/') {
+    return converted.substring(0, converted.length - 1);
+  }
+
+  return converted;
+}
+
+function formatField(field) {
+  const fieldFromParent = formatSelector(field);
+  if (fieldFromParent[0] === '/') {
+    return fieldFromParent.substring(1)
+  }
+
+  return fieldFromParent;
+
+}
+
+function groupBy(xs, getKey) {
+  return xs.reduce(function(rv, x) {
+    (rv[getKey(x)] = rv[getKey(x)] || []).push(x);
+    return rv;
+  }, {});
+};
+
+function groupByArray(xs, getKey) {
+  return xs.reduce(function(rv, x) {
+    (rv[getKey(x)] = rv[getKey(x)] || []).push(x);
+    return rv;
+  }, {});
+};
+
+module.exports = validateRels
