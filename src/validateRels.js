43b8598a5993960b75c40d6b56982fbce3a91009
Sven Hübner <sven.huebner.it@gmx.de>
2024-04-17 11:48:37 +0200

    feat: add keyRef feature

--- src/validateRels.js
+++ src/validateRels.js
@@ -5,11 +5,50 @@ function validateRels(objToValidate, relationsSpec) {
   const relationIds = Object.keys(relations)
   const keyIds = relationIds.filter(id => relations[id].key)
   const uniqueIds = relationIds.filter(id => relations[id].unique)
+  const keyrefIds = relationIds.filter(id => relations[id].keyRef)
 
   const keyDiagnostics = keyIds.flatMap(id => validateKey(id, objToValidate, relations[id].key))
   const uniqueDiagnostics = uniqueIds.flatMap(id => validateUnique(id, objToValidate, relations[id].unique))
+  const keyrefDiagnostics = keyrefIds.flatMap(id => validateKeyRef(id, objToValidate, relations[id].keyRef, relations[relations[id].keyRef.refer].key))
 
-  return keyDiagnostics.concat(uniqueDiagnostics)
+  return keyDiagnostics.concat(uniqueDiagnostics).concat(keyrefDiagnostics)
+}
+
+function validateKeyRef(id, objToValidate, refRelation, keyRelation) {
+  const { selector: refSelector, field: refField } = refRelation
+  const { selector: keySelector, field: keyField } = keyRelation
+  const refNodes = jp.nodes(objToValidate, refSelector);
+  const refNodesWithFields = refNodes.map(node => {
+    return {
+      ...node,
+      fieldNodes: jp.nodes(node.value, refField)
+    }
+  })
+  const refNodesWithUniqueField = refNodesWithFields.filter(f => f.fieldNodes.length > 0)
+
+  const keyNodes = jp.nodes(objToValidate, keySelector);
+  const keyNodesWithFields = keyNodes.map(node => {
+    return {
+      ...node,
+      fieldNodes: jp.nodes(node.value, keyField)
+    }
+  })
+  const keyNodesWithUniqueField = keyNodesWithFields.filter(f => f.fieldNodes.length > 0)
+  const keys = new Set(keyNodesWithUniqueField.map(n => n.fieldNodes[0].value))
+
+  const invalidRefNodes = refNodesWithUniqueField.filter(n => !keys.has(n.fieldNodes[0].value))
+
+  return invalidRefNodes.map(n => {
+    return {
+      instancePath: formatPathToNode(n),
+      keyword: "keyRef",
+      message: `invalid keyRef '${n.fieldNodes[0].value}'`,
+      params: {
+        relationId: id,
+        invalidRef: n.fieldNodes[0].value
+      }
+    }
+  })
 }
 
 function validateUnique(id, objToValidate, relation, keyword) {
@@ -97,13 +136,6 @@ function formatField(field) {
 }
 
 function groupBy(xs, getKey) {
-  return xs.reduce(function (rv, x) {
-    (rv[getKey(x)] = rv[getKey(x)] || []).push(x);
-    return rv;
-  }, {});
-};
-
-function groupByArray(xs, getKey) {
   return xs.reduce(function (rv, x) {
     (rv[getKey(x)] = rv[getKey(x)] || []).push(x);
     return rv;
